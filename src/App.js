c9c92893374eddb80f8159bdd9f9bf5d1ccb0562
Abhiram <abhirampandey54@gmail.com>
2025-05-14 13:32:30 +0530

    Callendar added

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Form from "./scenes/form";
 //import Pie from "./scenes/pie";
 //import FAQ from "./scenes/";
 //import Geography from "./scenes/geography";
-//import Calendar from "./scenes/calendar";
+import Calendar from "./scenes/calendar/calendar";
 
 
 function App() {
@@ -38,7 +38,7 @@ function App() {
               {/*<Route path="/line" element={<Line/>}/>*/}
               {/*<Route path="/faq" element={<FAQ/>}/>*/}
               {/*<Route path="/geography" element={<Geography/>}/>*/}
-              {/*<Route path="/calendar" element={<Calendar/>}/>*/}
+              <Route path="/calendar" element={<Calendar />}/>
             </Routes>    
         </main>
     </div>
